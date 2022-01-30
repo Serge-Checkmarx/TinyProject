7237cc3819db21650c7a6d5bc08e882825026211
Sergio <SOME_RANDOM_ALIAS@example.com>
2022-01-30 21:00:02 +0200

    Commit of login code

--- Projects/TinyProject/src/org/tiny/Main.java
+++ Projects/TinyProject/src/org/tiny/Main.java
@@ -4,9 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    // Small Change Serge
-    // Second Change Serge
-    // Third change
 
     }
+
+    public String login() {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String sql = "select * from users where (email ='" + email +"' and password ='" + password + "')";
+
+        Connection connection = pool.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        if (result.next()) {
+            loggedIn = true;
+            // Successfully logged in and redirect to user profile page
+        }
+        else
+        {
+            // Auth failure - Redirect to Login Page
+        }
+    }}
 }
