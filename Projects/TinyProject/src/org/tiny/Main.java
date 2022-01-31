dc8d295a496fc1dfedcdc3109e32a03d61e7b557
Sergio <SOME_RANDOM_ALIAS@example.com>
2022-01-31 13:24:14 +0200

    Code Initial

--- Projects/TinyProject/src/org/tiny/Main.java
+++ Projects/TinyProject/src/org/tiny/Main.java
@@ -10,22 +10,18 @@ public class Main {
     public String login() {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-        //String sql = "select * from users where (email ='" + email +"' and password ='" + password + "')";
-        String sql = "select * from users where email = ? and password = ? ";
+        String sql = "select * from users where (email ='" + email +"' and password ='" + password + "')";
+
         Connection connection = pool.getConnection();
-        //Statement statement = connection.createStatement();
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        //ResultSet result = statement.executeQuery(sql);
-        preparedStatement.setString(1, email);
-        preparedStatement.setString(2, password);
-        ResultSet result = preparedStatement.executeQuery();
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
         if (result.next()) {
             loggedIn = true;
             // Successfully logged in and redirect to user profile page
-        } else {
+        }
+        else
+        {
             // Auth failure - Redirect to Login Page
-            //OK
-            //ReOK
         }
     }
 }
