910f5627f85e8b53cca37f404c69627831ccb2af
Sergio <SOME_RANDOM_ALIAS@example.com>
2022-01-31 16:22:56 +0200

    Commit And Push Original Code A1

--- Projects/TinyProject/src/org/tiny/Main.java
+++ Projects/TinyProject/src/org/tiny/Main.java
@@ -7,21 +7,5 @@ public class Main {
 
     }
 
-    public String login() {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String sql = "select * from users where (email ='" + email +"' and password ='" + password + "')";
 
-        Connection connection = pool.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery(sql);
-        if (result.next()) {
-            loggedIn = true;
-            // Successfully logged in and redirect to user profile page
-        }
-        else
-        {
-            // Auth failure - Redirect to Login Page
-        }
-    }
 }
