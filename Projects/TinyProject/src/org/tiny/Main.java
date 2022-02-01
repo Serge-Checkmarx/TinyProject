f37945b82938dd585b14f0f63380c435427c7c8e
Sergio <SOME_RANDOM_ALIAS@example.com>
2022-02-01 16:15:35 +0200

    Fix SQL Injection in Login Functionnality

--- Projects/TinyProject/src/org/tiny/Main.java
+++ Projects/TinyProject/src/org/tiny/Main.java
@@ -24,7 +24,7 @@ public class Main {
             // Successfully logged in and redirect to user profile page
         }
         else {
-            // Auth failure - Redirect to Login Page
+            //Auth failure - Redirect to Login Page
         }
     }
 
